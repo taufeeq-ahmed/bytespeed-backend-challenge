cdf3f65543ad2c8ef5765ace3dff9cee74c2051e
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-28 19:18:07 +0530

    logic done

---
+++ src/helpers/connections.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const getEmailConnectedContacts = async (email: string) => {
+    const emailConnectedContacts = await prisma.contact.findMany({
+        where: {
+            email
+        }
+    })
+    return emailConnectedContacts
+}
+
+const getPhoneConnectedContacts = async (phoneNumber: string) => {
+    const phoneConnectedContacts = await prisma.contact.findMany({
+        where: {
+            phoneNumber
+        }
+    })
+    return phoneConnectedContacts
+}
+
+const getAllConnectedContacts = async (email: string, phoneNumber: string) => {
+    const allConnectedContacts = await prisma.contact.findMany({
+        where: {
+            OR: [
+                { email }, { phoneNumber }
+            ]
+        }
+    })
+    return allConnectedContacts
+}
+
+export {
+    getAllConnectedContacts,
+    getEmailConnectedContacts,
+    getPhoneConnectedContacts
+}
--- src/routes/identify.ts
+++ src/routes/identify.ts
@@ -1,15 +1,66 @@
 import express, { Request, Response } from "express";
+import { getAllConnectedContacts, getEmailConnectedContacts, getPhoneConnectedContacts } from "../helpers/connections";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 
 const router = express.Router()
 
 const containsnewData = async (email: string, phoneNumber: string): Promise<boolean> => {
-    // tells if there is any new data in the body
-    return true
+    const emailConnectedContacts = await getEmailConnectedContacts(email)
+    const phoneConnectedContacts = await getPhoneConnectedContacts(phoneNumber)
+
+    return emailConnectedContacts.length === 0 || phoneConnectedContacts.length === 0
 }
 
 const consolidateContacts = async (email: string, phoneNumber: string) => {
-    // tells if there is any new data in the body
-    return true
+    const allConnectedContacts = await getAllConnectedContacts(email, phoneNumber)
+
+    let primaryContactId: number = -1
+    const emails: string[] = []
+    const phoneNumbers: string[] = []
+    const secondaryContactIds: number[] = []
+
+    allConnectedContacts.forEach((contact) => {
+        const { email, phoneNumber, linkPrecedence, id } = contact
+
+        if (linkPrecedence === "primary") {
+            primaryContactId = id
+        } else {
+            secondaryContactIds.push(id)
+        }
+
+        if (email) emails.push(email)
+        if (phoneNumber) phoneNumbers.push(phoneNumber)
+
+    })
+
+    const contact = {
+        primaryContactId,
+        emails,
+        phoneNumbers,
+        secondaryContactIds
+    }
+    return {
+        contact
+    }
+}
+
+const addContact = async (email: string, phoneNumber: string) => {
+    const allConnectedContacts = await getAllConnectedContacts(email, phoneNumber)
+
+    const linkPrecedence = allConnectedContacts.length === 0 ? "primary" : "secondary"
+    const linkedId = linkPrecedence === "primary" ? null : allConnectedContacts[0].id
+
+    const contact = await prisma.contact.create({
+        data: {
+            email,
+            phoneNumber,
+            linkedId,
+            linkPrecedence
+        },
+    })
 }
 
 router.post("/identify",
@@ -18,13 +69,13 @@ router.post("/identify",
         const { email = null, phoneNumber = null } = req.body
 
         if (!email && !phoneNumber) {
-            res.status(400).send({ message: "send email/phoneNumber to identify the user" })
+            return res.status(400).send({ message: "send email/phoneNumber to identify the user" })
         }
 
         const hasNewData = await containsnewData(email, phoneNumber)
 
         if (hasNewData) {
-            // take body and create a new contact in the table
+            await addContact(email, phoneNumber)
         }
 
         const fullContact = await consolidateContacts(email, phoneNumber)
